7d9fc1efa27857f4f079d4e8be698d2a84b5644e
franklin <franklin@franklins-MacBook-Air.local>
2024-01-10 23:50:36 +0100

    job details

--- src/Pages/JobDetails.jsx
+++ src/Pages/JobDetails.jsx
@@ -1,36 +1,40 @@
-import {useState,useEffect} from 'react';
-import { useParams} from 'react-router-dom';
-import Swal from 'sweetalert2'
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import Swal from "sweetalert2";
+import PageHeader from "../components/PageHeader";
 
 const JobDetails = () => {
-    const {id} = useParams();
-    const [job, setJob] = useState([]);
+  const { id } = useParams();
+  const [job, setJob] = useState([]);
 
-    useEffect(() => {
-    fetch(`http://localhost:3000/all-jobs/${id}`).then(res => res.json()).then(data => setJob(data))
-    }, [])
+  useEffect(() => {
+    fetch(`http://localhost:3000/all-jobs/${id}`)
+      .then((res) => res.json())
+      .then((data) => setJob(data));
+  }, []);
 
-    const handleApply = async() => {
-        const {value: url} = await Swal.fire({
-            input: 'url',
-            inputLabel: 'URL address',
-            inputPlaceholder: 'Enter the url'
-        });
-
-        if(url) {
-            Swal.fire(`Entered URL: $(url)`);
-        }
-
+  const handleApply = async () => {
+    const { value: url } = await Swal.fire({
+      input: "url",
+      inputLabel: "URL address",
+      inputPlaceholder: "Enter the url",
+    });
+    if (url) {
+      Swal.fire(`Entered URL: ${url}`);
     }
+  };
 
   return (
-    <div className='max-w-screen-2xl container mx-auto xl:px-24 px-4'>
+    <div className="max-w-screen-2xl container mx-auto xl:px-24 px-4">
+      <PageHeader title={"Single Job Page"} path={"single job"} />
       <h2>JobDetails:{id}</h2>
       <h1>{job.jobTitle}</h1>
 
-      <button className='bg-green px-8 py-2 text-white' onClick={handleApply}>Apply Now</button>
+      <button className="bg-green px-8 py-2 text-white" onClick={handleApply}>
+        Apply Now
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default JobDetails
+export default JobDetails;
